39980c1943737e87d8977c6c17fbdb493b19e13f
Erdi Özbek <erdi.ozbek@applogist.com>
2019-04-09 00:17:12 +0300

    Project Start

---
+++ app/src/main/java/com/ei/lyrebirddemo/LyreBirdApp.java
@@ -0,0 +1,19 @@
+package com.ei.lyrebirddemo;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+public class LyreBirdApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
